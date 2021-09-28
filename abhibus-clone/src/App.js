0065e740a92898b5e724472a9f96eb06a945c027
nagendrax02 <rawatnagendra98@gmail.com>
2021-09-28 11:25:56 +0530

    initial commit

---
+++ abhibus-clone/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="App">
+        <button className='btn btn-primary'>Click me</button>
+    </div>
+  );
+}
+
+export default App;
